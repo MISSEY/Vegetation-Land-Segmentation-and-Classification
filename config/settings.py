142bcc4fafd9851dc9b7ae2496d81ca3292d457c
Saurabh Mishra <skmgun@gmail.com>
2021-04-30 17:05:29 +0200

    Update settings

--- config/settings.py
+++ config/settings.py
@@ -13,8 +13,8 @@ underscore = '_'
 
 # while training on cluster
 
-check_point_output_directory = '/netscratch/smishra/thesis/output' + underscore + str(config.image_size) + underscore + str(
-    config.model_name)
+check_point_output_directory = '/netscratch/smishra/thesis/output/output' + underscore + str(config.image_size) + underscore + str(
+    config.model_name) + underscore + str(config._version_name)
 data_directory_cluster = '/netscratch/smishra/thesis/vegetation_dataset'
 
 weights_directory = '/netscratch/smishra/output'
