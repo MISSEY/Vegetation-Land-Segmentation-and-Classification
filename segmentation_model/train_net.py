fb0d41defb12b1a1a11c8a9d3474bee3cd21e19b
Saurabh Mishra <skmgun@gmail.com>
2021-05-06 18:51:06 +0200

    experiment8

--- segmentation_model/train_net.py
+++ segmentation_model/train_net.py
@@ -233,26 +233,28 @@ class BasePredictor(engine.DefaultPredictor):
             return predictions[0]
 
 
-
 def custom_mapper(dataset_dict):
     # Implement a mapper, similar to the default DatasetMapper, but with your own customizations
     dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
     image = utils.read_image(dataset_dict["file_name"], format="BGR")
-    transform_list = [T.RandomBrightness(0.5, 2),
-                      T.RandomContrast(0.5, 2),
-                      T.RandomSaturation(0.5, 2),
-                      T.RandomFlip(prob=0.5, horizontal=False, vertical=True),
-                      T.RandomFlip(prob=0.5, horizontal=True, vertical=False),
-                      ]
-    image, transforms = T.apply_transform_gens(transform_list, image)
-    dataset_dict["image"] = torch.as_tensor(image.transpose(2, 0, 1).astype("float32"))
+    augs = T.AugmentationList([
+        T.RandomBrightness(0.5, 2),
+        T.RandomContrast(0.5, 2),
+        T.RandomSaturation(0.5, 2),
+        T.RandomFlip(prob=0.5, horizontal=False, vertical=True),
+        T.RandomFlip(prob=0.5, horizontal=True, vertical=False)
+    ])
+
+    auginput = T.AugInput(image)
+    transform = augs(auginput)
+    image = torch.from_numpy(auginput.image.transpose(2, 0, 1))
+    dataset_dict["image"] = image
 
     annos = [
-        utils.transform_instance_annotations(obj, transforms, image.shape[:2])
+        utils.transform_instance_annotations(obj, [transform], image.shape[1:])
         for obj in dataset_dict.pop("annotations")
-        if obj.get("iscrowd", 0) == 0
     ]
-    instances = utils.annotations_to_instances(annos, image.shape[:2])
+    instances = utils.annotations_to_instances(annos, image.shape[1:])
     dataset_dict["instances"] = utils.filter_empty_instances(instances)
     return dataset_dict
 
