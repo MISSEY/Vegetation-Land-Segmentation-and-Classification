e49d4dae489cd48ef9d3804d4ccda4c2b03af6d3
Saurabh Mishra <skmgun@gmail.com>
2021-05-11 18:42:00 +0200

    experiment23

--- config/config.py
+++ config/config.py
@@ -12,7 +12,7 @@ _preprocessed_denamark_shape_files_ = 'Preprocessed_denamark_shape_files'
 
 
 # change version name while training
-_version_name ='v_Jan_Mar'
+_version_name ='v_Jul_Sep'
 
 _version_train_ = 'train'
 _version_validation_ = 'validation'
@@ -22,7 +22,7 @@ _version_crop_images_ = 'cropped_images'
 # Image
 # Image configuration depend upon the data version above v_3 : 800 x 800, v_5 : 400 x 400, v_6 : 200 x 200
 
-train_image_size = 400
+train_image_size = 800
 test_image_size = 800
 
 instance_per_image_filter = 2000
