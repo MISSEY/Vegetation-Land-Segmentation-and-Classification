e46bd057b7812269dff9bf36b237a2d223727dd1
Saurabh Mishra <skmgun@gmail.com>
2021-06-04 19:00:11 +0200

    Experiment 64

--- segmentation_model/train_net.py
+++ segmentation_model/train_net.py
@@ -325,7 +325,7 @@ def custom_mapper(dataset_dict):
     dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
     image = utils.read_image(dataset_dict["file_name"], format="BGR")
     augs = T.AugmentationList([
-        # T.Resize((448,448),interp=Image.LANCZOS),
+        T.Resize((224,224),interp=Image.LANCZOS),
         # T.RandomBrightness(0.5, 2),
         # T.RandomContrast(0.5, 2),
         # T.RandomSaturation(0.5, 2),
