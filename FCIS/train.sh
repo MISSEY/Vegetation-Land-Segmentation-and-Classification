a92f691f7d299261ccc3d821a4ed9766b1fe3c87
missey <skmgun@gmail.com>
2021-07-05 09:35:39 +0200

    Fix readme

--- FCIS/train.sh
+++ FCIS/train.sh
@@ -10,21 +10,21 @@ rm lib/utils/load_data.py
 cp /home/smishra/fcis/1/FCIS_mxnet/lib/utils/load_data.py lib/utils/
 
 rm fcis/core/module.py
-cp /home/smishra/fcis/11/FCIS_mxnet/fcis/core/module.py fcis/core/
+cp /home/smishra/fcis/1/FCIS_mxnet/fcis/core/module.py fcis/core/
 
 rm fcis/function/test_fcis.py
-cp /home/smishra/fcis/10/FCIS_mxnet/fcis/function/test_fcis.py fcis/function/
+cp /home/smishra/fcis/1/FCIS_mxnet/fcis/function/test_fcis.py fcis/function/
 
 rm fcis/train_end2end.py
-cp /home/smishra/fcis/10/FCIS_mxnet/fcis/train_end2end.py fcis/
+cp /home/smishra/fcis/1/FCIS_mxnet/fcis/train_end2end.py fcis/
 
 rm experiments/fcis/cfgs/resnet_v1_101_coco_fcis_end2end_ohem.yaml
-cp /home/smishra/fcis/10/FCIS_mxnet/experiments/fcis/cfgs/resnet_v1_101_coco_fcis_end2end_ohem.yaml experiments/fcis/cfgs/
+cp /home/smishra/fcis/1/FCIS_mxnet/experiments/fcis/cfgs/resnet_v1_101_coco_fcis_end2end_ohem.yaml experiments/fcis/cfgs/
 
 cat experiments/fcis/cfgs/resnet_v1_101_coco_fcis_end2end_ohem.yaml
 pwd
 
 rm fcis/config/config.py
-cp /home/smishra/fcis/10/FCIS_mxnet/fcis/config/config.py fcis/config/
+cp /home/smishra/fcis/1/FCIS_mxnet/fcis/config/config.py fcis/config/
 
 python experiments/fcis/fcis_end2end_train_test.py --cfg "experiments/fcis/cfgs/resnet_v1_101_coco_fcis_end2end_ohem.yaml"
